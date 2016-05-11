c89434d496e6b54fad9cad27f5f32ce7b69d1ef0
Sean Behan <inbox@seanbehan.com>
2016-05-11 01:02:30 -0400

    add database

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template as render, jsonify
+from flask import Flask, render_template as render, jsonify, request
+from records import Database
 
+db = Database('postgres://localhost/flask_riotjs_starter')
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,8 +11,21 @@ def home():
 @app.route("/items.json")
 def items():
     return jsonify(
-        items=[
-            {'body': 'hello world'},
-            {'body': 'goodnight moon'}
-        ]
+        items=[row.as_dict() for row in db.query('select * from items')]
+    )
+
+@app.route("/items.json", methods=["POST"])
+def add_item():
+    db.query('insert into items (body) values (:body)', body=request.values.get('body'))
+
+    return jsonify(
+        item={'body': request.values.get('body')}
+    )
+
+@app.route("/items.json", methods=["DELETE"])
+def delete_item():
+    db.query('delete from items where body = :body', body=request.values.get('body'))
+
+    return jsonify(
+        message="Item deleted"
     )
